605dad5a15e045e5f14ebe87b2f6b08e997d3f06
jyh123456 <1102291621@qq.com>
2019-03-14 22:44:23 +0800

    2.24

---
+++ 江宇航2.24/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter a number:";
+    cin>>a;
+    b=a%2;
+    if(b==0)
+        cout<<"Å¼Êý";
+    else
+        cout<<"ÆæÊý";
+
+
+    return 0;
+}
