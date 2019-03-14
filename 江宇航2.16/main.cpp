1e8fb549b64533f223ab463fa15cab4dac38c847
jyh123456 <1102291621@qq.com>
2019-03-14 18:33:52 +0800

    2.16

---
+++ 江宇航2.16/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double number1,number2;
+    cout<<"Enter two numbers";
+    cin>>number1>>number2;
+    cout<<"ºÍ£º"<<number1+number2<<endl;
+    cout<<"²î£º"<<number1-number2<<endl;
+    cout<<"»ı£º"<<number1*number2<<endl;
+    cout<<"ÉÌ£º"<<number1/number2<<endl;
+    return 0;
+}
